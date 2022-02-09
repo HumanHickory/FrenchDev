e20fa26e688e40e7aaaf0f17f22afdd8a5289963
HumanHickory <HickoryWebDesigns@gmail.com>
2022-02-09 11:35:48 -0500

    added news and trying to fix mobile issues

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { VerbService } from '../Services/VerbServices';
+import { MessageService } from 'primeng/api';
+import { Verb } from '../Models/Verb';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  providers: [MessageService]
 })
 export class NewsComponent implements OnInit {
+  Verbs: Array<Verb> = [];
 
-  constructor() { }
+  constructor(private verbService: VerbService,
+    private messageService: MessageService
+    ) { }
 
   ngOnInit(): void {
+    this.GetRecentlyAddedVerbs();
+  }
+
+  GetRecentlyAddedVerbs(){
+    return this.verbService.getRecentlyAdded().subscribe((verbs) => {
+        this.Verbs = verbs;
+    }, error => {
+      this.messageService.add({ severity: 'warning', summary: 'Failed To Get Verbs', detail: 'Could not retrieve recently added verbs.' });
+
+    });
   }
 
 }
